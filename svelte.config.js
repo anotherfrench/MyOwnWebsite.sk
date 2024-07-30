f1fbdefe6939c1ce643d0bd9af7a2c6e2a4007df
CodeKyana <169302941+anotherfrench@users.noreply.github.com>
2024-07-30 22:42:51 +0200

    Remove bad config adapter

--- svelte.config.js
+++ svelte.config.js
@@ -7,18 +7,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			},
-			platformProxy: {
-				configPath: 'wrangler.toml',
-				environment: undefined,
-				experimentalJsonConfig: false,
-				persist: false
-			}
-		})
+		adapter: adapter()
 	},
 	inlineStyleThreshold: Infinity,
 	preprocess: vitePreprocess()
