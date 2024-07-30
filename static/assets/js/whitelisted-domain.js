6cb4e02d1ee27f321d8e202fa19f77288dfce5cf
CodeKyana <169302941+anotherfrench@users.noreply.github.com>
2024-07-30 22:28:54 +0200

    Fix WL URL

--- static/assets/js/whitelisted-domain.js
+++ static/assets/js/whitelisted-domain.js
@@ -1,7 +1,7 @@
-var re = /^(anotherfrench\.(github)\.io|(localhost))$/;
+var re = /^(anotherfrench\.(pages)\.dev|(localhost))$/;
 if (!re.test(location.hostname)) {
 	console.error(`[local] Connected via a unwhitelisted domain name: ${location.hostname}`);
-	location.href = 'https://anotherfrench.github.io';
+	location.href = 'https://anotherfrench.pages.dev';
 } else {
 	console.info(`[local] Connected via a whitelisted domain name: ${location.hostname}`);
 }
