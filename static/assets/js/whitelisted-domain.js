41ba7c7190d9cd4aded26039a74e2cf7762439e6
CodeKyana <169302941+anotherfrench@users.noreply.github.com>
2024-07-30 23:20:45 +0200

    Change WL Domain & Integrate it

--- static/assets/js/whitelisted-domain.js
+++ static/assets/js/whitelisted-domain.js
@@ -1,7 +1,7 @@
-var re = /^(anotherfrench\.(pages)\.dev|(localhost))$/;
+var re = /^(anotherfrench\.(vercel)\.app|(localhost))$/;
 if (!re.test(location.hostname)) {
 	console.error(`[local] Connected via a unwhitelisted domain name: ${location.hostname}`);
-	location.href = 'https://anotherfrench.pages.dev';
+	location.href = 'https://anotherfrench.vercel.app';
 } else {
 	console.info(`[local] Connected via a whitelisted domain name: ${location.hostname}`);
 }
